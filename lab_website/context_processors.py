4af83b8bb60fac8e5e9fb2b7511383b1b9992120
Dave Bridges <dave.bridges@gmail.com>
2026-02-11 16:11:40 -0500

    Added link to API for publications, part of issue #54

--- lab_website/context_processors.py
+++ lab_website/context_processors.py
@@ -8,11 +8,12 @@ def lab_metadata(request):
 
     # Get the LabAddress object first
     lab_addr_obj = LabAddress.objects.filter(type="Primary").select_related('address').first()
-    
+
     return {
         'lab_name': settings.LAB_NAME,
         'twitter_handle': settings.TWITTER_NAME,
         'facebook_handle': settings.FACEBOOK_NAME,
         'google_plus_id': settings.GOOGLE_PLUS_ID,
         'actual_address': lab_addr_obj.address if lab_addr_obj else None,
+        'api_url': f"{request.scheme}://{request.get_host()}/api/v2/",
     }
