6cfd782f40bd9d977a2d84742d1aa59a9e082b46
Dave Bridges <dave.bridges@gmail.com>
2026-02-11 15:54:22 -0500

    Added generic JSON-LD context to base template, so it shows in every view (part of issue #34)

--- lab_website/context_processors.py
+++ lab_website/context_processors.py
@@ -5,10 +5,14 @@ def lab_metadata(request):
     """
     Injects global lab data into every template context.
     """
+
+    # Get the LabAddress object first
+    lab_addr_obj = LabAddress.objects.filter(type="Primary").select_related('address').first()
+    
     return {
         'lab_name': settings.LAB_NAME,
         'twitter_handle': settings.TWITTER_NAME,
         'facebook_handle': settings.FACEBOOK_NAME,
         'google_plus_id': settings.GOOGLE_PLUS_ID,
-        'lab_address': LabAddress.objects.filter(type="Primary").first(),
+        'actual_address': lab_addr_obj.address if lab_addr_obj else None,
     }
